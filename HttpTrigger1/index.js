26af6f818a9df0608072c701e04ccf69b273bba3
darshan chauhan <dvchauhan5566@gmail.com>
2025-03-29 18:03:30 +0530

    Refactor HTTP trigger function and remove unnecessary server file

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -2,13 +2,8 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? `Hello, ${name}. This is a Hello World Azure Function!`
-        : "Please pass a name on the query string or in the request body.";
-
     context.res = {
         status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: "Hello, World!"
     };
 };
